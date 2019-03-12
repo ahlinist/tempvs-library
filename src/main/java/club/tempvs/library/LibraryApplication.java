5cfe542be5d22afaa7a427fcbdb9e4e11ebe4f42
Anton Hlinisty <anton.hlinisty@gmail.com>
2019-03-12 23:42:18 +0300

    #7. Remove zuul dependencies

--- src/main/java/club/tempvs/library/LibraryApplication.java
+++ src/main/java/club/tempvs/library/LibraryApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -20,7 +19,6 @@ import org.springframework.web.client.RestTemplate;
 @EnableJpaAuditing
 @EnableCircuitBreaker
 @EnableEurekaClient
-@EnableZuulProxy
 @SpringBootApplication
 public class LibraryApplication {
 
