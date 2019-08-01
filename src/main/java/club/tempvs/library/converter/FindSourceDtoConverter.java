4602e9f36630fcd2ed7c1aa62682b2ec10a1dd84
Anton Hlinisty <anton.hlinisty@gmail.com>
2019-08-02 01:14:34 +0300

    UrlDecode search query

--- src/main/java/club/tempvs/library/converter/FindSourceDtoConverter.java
+++ src/main/java/club/tempvs/library/converter/FindSourceDtoConverter.java
@@ -5,12 +5,15 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriUtils;
 
 import java.util.Base64;
 
 @Component
 @RequiredArgsConstructor
 public class FindSourceDtoConverter implements Converter<String, FindSourceDto> {
+
+    private static final String ENCODING = "UTF-8";
 
     private final ObjectMapper objectMapper;
 
@@ -19,7 +22,8 @@ public class FindSourceDtoConverter implements Converter<String, FindSourceDto> 
         try {
             byte[] decodedBytes = Base64.getDecoder().decode(q);
             String decodedString = new String(decodedBytes);
-            return objectMapper.readValue(decodedString, FindSourceDto.class);
+            String uriDecodedString = UriUtils.decode(decodedString, ENCODING);
+            return objectMapper.readValue(uriDecodedString, FindSourceDto.class);
         } catch (Exception e) {
             throw new IllegalArgumentException(e);
         }
